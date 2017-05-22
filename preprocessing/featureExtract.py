0f2157672b75ee4a5931ca5fdfde2f6d705410d1
Coquelicots <greazepasta@gmail.com>
2017-05-22 22:03:02 +0800

    not clear

--- preprocessing/featureExtract.py
+++ preprocessing/featureExtract.py
@@ -11,21 +11,20 @@ def compare(a,b):
 		return cmp(a,b)
 	return cmp(len(a),len(b))
 
-dataset_path = '../Data/classical/'
+dataset_path = '../Data/pop/'
 train_fns = [fn for fn in listdir(dataset_path)]
 train_fns.sort(cmp = compare)
 print train_fns
 
 
-count = 0
+count = 1
 data = []
 for fn in train_fns:
 	y,sr = librosa.core.load((dataset_path + fn),mono = True)
 	D = librosa.stft(y)
 	S = librosa.amplitude_to_db(D,ref=np.max)
-	#S, phase = librosa.magphase(librosa.stft(y))
 	plt.figure()
 	librosa.display.specshow(S)
-	plt.savefig('../Pic/'+str(count)+'.png',format='png')
+	plt.savefig('../Pic/pop/'+str(count)+'.png',format='png')
 	plt.close()
 	count+=1
--- preprocessing/rescaleImage.py
+++ preprocessing/rescaleImage.py
@@ -9,7 +9,7 @@ def compare(a,b):
 		return cmp(a,b)
 	return cmp(len(a),len(b))
 
-dataset_path = 'Pic/'
+dataset_path = '../Pic/rock/'
 train_fns = [fn for fn in listdir(dataset_path)]
 train_fns.sort(cmp = compare)
 
@@ -18,5 +18,5 @@ data = []
 for fn in train_fns:
 	d = Image.open((dataset_path + fn))
 	d = d.resize((256,256),Image.BILINEAR)
-	d.save(("SmallPic/"+fn))
+	d.save(("../SmallPic/rock/"+fn))
 	del d
