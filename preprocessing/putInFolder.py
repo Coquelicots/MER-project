8ff889fd2b4b61e860857a14b53ec122120e9a9c
Coquelicots <greazepasta@gmail.com>
2017-05-22 11:22:29 +0800

    using pure image

---
+++ preprocessing/putInFolder.py
@@ -0,0 +1,24 @@
+import pylab as pl
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import shutil
+
+def compare(a,b):
+	if len(a) == len(b):
+		return cmp(a,b)
+	return cmp(len(a),len(b))
+
+dataset_path = '../SmallPic/'
+des_path = '../picIntype/'
+des_folders = ['calm','joy','nostalgia','tenderness','solemnity','power','sadness','tension']
+train_fns = [fn for fn in listdir(dataset_path)]
+train_fns.sort(cmp = compare)
+
+tags = np.genfromtxt('../Data/classical_tag.txt', delimiter=',')
+tags = tags.astype('int')
+
+for x in range(len(tags)):
+	srcfile = dataset_path+train_fns[x]
+	dstdir = des_path + des_folders[tags[x]]
+	shutil.copy(srcfile, dstdir)
---
+++ preprocessing/rescaleImage.py
@@ -0,0 +1,22 @@
+import pylab as pl
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import PIL.Image as Image
+
+def compare(a,b):
+	if len(a) == len(b):
+		return cmp(a,b)
+	return cmp(len(a),len(b))
+
+dataset_path = 'Pic/'
+train_fns = [fn for fn in listdir(dataset_path)]
+train_fns.sort(cmp = compare)
+
+count = 0
+data = []
+for fn in train_fns:
+	d = Image.open((dataset_path + fn))
+	d = d.resize((256,256),Image.BILINEAR)
+	d.save(("SmallPic/"+fn))
+	del d
